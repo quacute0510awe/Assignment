3773a7afafb6d9e4e7283c49390e88bae75a8d64
Nguyen Ha Trang <101710561+quacute0510awe@users.noreply.github.com>
2022-11-07 19:50:20 +0700

    Update DBContext.java

--- AssignPRJ301/src/java/dal/DBContext.java
+++ AssignPRJ301/src/java/dal/DBContext.java
@@ -13,7 +13,7 @@ public class DBContext {
     public DBContext() {
         try {
             String user = "sa";
-            String pass = "sa";
+            String pass = "12345";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ301_ASSGN";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
